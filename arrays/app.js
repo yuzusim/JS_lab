028109ae70ab22bfff04cca6e46441f27857847a
yuzusim <yuzusim@gmail.com>
2024-12-04 14:02:18 +0900

    arrays 완료

---
+++ arrays/app.js
@@ -0,0 +1,24 @@
+// const mon = "mon";
+// const tue = "tue";
+// const wed = "wed";
+// const thu = "thu";
+// const fri = "fri";
+// const sat = "sat";
+// const sun = "sun";
+
+// const daysOfweek = mon + tue + wed + thu + fri + sat + sun;
+// const daysOfweek = [mon , tue , wed , thu , fri , sat , sun];
+// console.log(daysOfweek);
+
+
+const daysOfweek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+console.log(daysOfweek);
+
+// 항목 받아옴
+console.log(daysOfweek[4]);
+
+// 요일 추가
+daysOfweek.push("sun");
+console.log(daysOfweek);
+
