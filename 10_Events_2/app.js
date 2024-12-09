f901b9fbbd581af84ffd792474fdbeff10234f3c
yuzusim <yuzusim@gmail.com>
2024-12-09 17:19:55 +0900

    MouseEvent

---
+++ 10_Events_2/app.js
@@ -0,0 +1,26 @@
+// 1) 복귀
+// listen하고 싶은 event를 찾는 가장 좋은 방법은, 구글에 찾고 싶은 element의 이름, 예를들어 h1 html element mdn을 검색.
+// 우리는 javascript의 element를 원하니, 링크에 Web APIs라는 문장이 포함된 페이지를 찾아. 왜냐면 이건 JS관점의 HTML Heading Element란 의미야.
+// 너무 복잡하면 element를 console.dir로 출력해서 on~ 이라고 적혀있는걸 사용하면 됨.
+
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick(){
+    title.style.color = "blue";
+}
+
+function handleMouseEnter() {
+    // console.log("Mouse is here")
+    title.innerText = "Mouse is here!"
+}
+
+function handleMouseLeave() {
+    // console.log("Mouse is gone")
+    title.innerText = "Mouse is gone!"
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+// 하지만 대부분의 경우에는 style은 CSS를 통해 변경되어야 해. 우리는 CSS파일을 어떻게 사용하지는 배워볼거야.
