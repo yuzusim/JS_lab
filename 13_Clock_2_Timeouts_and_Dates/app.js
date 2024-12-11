d34153bb91a968a9e28aca40704a4080a0b0f800
yuzusim <yuzusim@gmail.com>
2024-12-11 17:42:01 +0900

    clock 초, 문자로 채움 -> padStart

--- 13_Clock_2_Timeouts_and_Dates/app.js
+++ 13_Clock_2_Timeouts_and_Dates/app.js
@@ -1,17 +1,52 @@
-const colck = document.querySelector("h2#clock");
-// const clock = document.querySelector("#clock");
-// const clock = document.getElementById("clock");
-// 모두 같다.
+// const colck = document.querySelector("h2#clock");
+// // colck.innerText = "lalala";
 
-// colck.innerText = "lalala";
-
-function sayHello(){
-    console.log("hello");
-}
+// function sayHello(){
+//     console.log("hello");
+// }
 // 이 함수를 2초마다 실행
 
-setInterval(sayHello, 5000)
+// 1)
+// setInterval(sayHello, 5000)
 // Interval (매번 일어나야 하는 무언가) -> 한 함수를 특정 시간마다 실행함
 // 첫번째 실행하고자 하는 펑션 -> sayHello
 // 두번째 아규먼트에 호출되는 펑션 간격을 몇 ms 할지 쓰면 됨 -> 5초
 
+// 2)
+// setTimeout(sayHello, 1000);
+// 1초 기다렸다가 한번만 실행
+
+// 시간이랑 분을 보여줄꺼야
+// Date object 라는 멋진 툴을 써볼께 (콘솔창네 써서 확인해봄)
+// Date 
+// new Date()
+
+// const date = new Date();
+// date.getDate();
+// date.getDay();
+// date.getFullYear();
+// date.getHours();
+// date.getMinutes();
+// date.getSeconds();
+
+// 3)
+// 그럼 보여줘야 할게
+// getHours, getMinutes, getSeconds
+// 뭔가 계속 실행하고 싶을때 Interval 써야함
+
+const colck = document.querySelector("h2#clock");
+
+function getClock(){
+   const date = new Date(); // 객체로 현재 날짜와 시간을 가져옴
+   // console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+   // console.log 대신 innerText로!!
+   colck.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+getClock() // getClock()을 즉시 호출
+setInterval(getClock, 1000); // 매초 마다 getClock()을 다시 실행
+
+// 00:00:0
+// 00:00:1 말고 -->  00:00:01
+// fomating 하고 싶어 그래서 getClock()을 호출할 건데 --> 다음 시간!
+// 웹사이트 페이지 로드시 getClock()을 실행하고 또 매초마다 다시 실행
