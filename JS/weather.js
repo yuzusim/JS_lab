221bf2a09ed763dda613b6c634ea6e2d3b8d6dce
yuzusim <yuzusim@gmail.com>
2025-02-17 14:25:29 +0900

    Weather 셋팅

--- JS/weather.js
+++ JS/weather.js
@@ -1,7 +1,6 @@
 // 이 함수는 유저의 위치를 줘 (Geolocation 지리적 위치)
 // 그리고 화면에 날씨를 보여줄꺼임 --> 한번 해보고 싶었음
 
-
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
@@ -19,3 +18,4 @@ function onGeoErrer(){
 // 2. 에러가 발생 했을때 실행될 함수
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoErrer);
 // onGeoOk({sexyObj});
+
